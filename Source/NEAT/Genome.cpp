1cc0d061ef5afd77dfcb4c3d692317f8c501bc4e
Kohei Nagasawa <k.nagasawa0124@gmail.com>
2021-01-18 10:15:09 +0900

    WIP Adding Genome, Generation and NEAT classes.

--- Source/NEAT/Genome.cpp
+++ Source/NEAT/Genome.cpp
@@ -8,57 +8,66 @@
 
 #include <NEAT/Genome.h>
 
-InnovationId InnovationCounter::getNewInnovationId()
+namespace NEAT
 {
-    InnovationId idOut = m_innovationCount;
-    m_innovationCount = m_innovationCount.val() + 1;
-    return idOut;
-}
-
-void InnovationCounter::reset()
-{
-    m_innovationCount = 0;
-}
-
-Genome::Genome(const Cinfo& cinfo)
-{
-    assert(cinfo.m_numInputNode > 0 && cinfo.m_numOutputNode > 0);
-
-    Network::Nodes nodes;
-    Network::Edges edges;
-    Network::NodeIds outputNodes;
-
-    const int numNodes = cinfo.m_numInputNode + cinfo.m_numOutputNode;
-
-    nodes.reserve(numNodes);
-    for (int i = 0; i < numNodes; i++)
+    InnovationId InnovationCounter::getNewInnovationId()
     {
-        nodes[i] = Node();
+        InnovationId idOut = m_innovationCount;
+        m_innovationCount = m_innovationCount.val() + 1;
+        return idOut;
     }
 
-    const int numEdges = cinfo.m_numInputNode * cinfo.m_numOutputNode;
-    edges.reserve(numEdges);
-    m_innovations.reserve(numEdges);
+    Genome::Genome(const Cinfo& cinfo)
     {
-        EdgeId eid(0);
-        InnovationId iid(0);
-        for (int i = 0; i < cinfo.m_numInputNode; i++)
+        assert(cinfo.m_numInputNode > 0 && cinfo.m_numOutputNode > 0);
+
+        Network::Nodes nodes;
+        Network::Edges edges;
+        Network::NodeIds outputNodes;
+
+        const int numNodes = cinfo.m_numInputNode + cinfo.m_numOutputNode;
+
+        nodes.reserve(numNodes);
+        for (int i = 0; i < numNodes; i++)
         {
-            for (int j = 0; j < cinfo.m_numOutputNode; j++)
+            nodes[i] = Node();
+        }
+
+        const int numEdges = cinfo.m_numInputNode * cinfo.m_numOutputNode;
+        edges.reserve(numEdges);
+        m_innovations.reserve(numEdges);
+        {
+            EdgeId eid(0);
+            InnovationId iid(0);
+            for (int i = 0; i < cinfo.m_numInputNode; i++)
             {
-                edges[eid] = Network::Edge(NodeId(i), NodeId(cinfo.m_numInputNode + j));
-                m_innovations.push_back({ iid, eid });
-                eid = eid.val() + 1;
-                iid = iid.val() + 1;
+                for (int j = 0; j < cinfo.m_numOutputNode; j++)
+                {
+                    edges[eid] = Network::Edge(NodeId(i), NodeId(cinfo.m_numInputNode + j));
+                    m_innovations.push_back({ iid, eid });
+                    eid = eid.val() + 1;
+                    iid = iid.val() + 1;
+                }
             }
         }
+
+        outputNodes.reserve(cinfo.m_numOutputNode);
+        for (int i = 0; i < cinfo.m_numOutputNode; i++)
+        {
+            outputNodes.push_back(NodeId(cinfo.m_numInputNode + i));
+        }
+
+        m_network = std::make_shared<Network>(nodes, edges, outputNodes);
     }
 
-    outputNodes.reserve(cinfo.m_numOutputNode);
-    for (int i = 0; i < cinfo.m_numOutputNode; i++)
+    float Genome::Node::getValue() const 
     {
-        outputNodes.push_back(NodeId(cinfo.m_numInputNode + i));
+        return m_value; 
     }
 
-    m_network = std::make_shared<Network>(nodes, edges, outputNodes);
+    void Genome::Node::setValue(float value)
+    {
+        assert(m_activation);
+        m_value = m_activation->activate(value);
+    }
 }
--- Source/NEAT/Genome.h
+++ Source/NEAT/Genome.h
@@ -7,64 +7,141 @@
 #pragma once
 
 #include <memory>
-#include <cstdint>
+#include <functional>
 
 #include <NEAT/MutableNetwork.h>
 
 DECLARE_ID(InnovationId, uint64_t);
+DECLARE_ID(GenomeId);
+DECLARE_ID(GenerationId);
 
-// Singleton class to control InnovationId.
-class InnovationCounter
+namespace NEAT
 {
-public:
-    InnovationCounter() = default;
-
-    InnovationId getNewInnovationId();
-
-    void reset();
-
-protected:
-    InnovationCounter(const InnovationCounter&) = delete;
-    void operator=(const InnovationCounter&) = delete;
-
-    InnovationId m_innovationCount = 0;
-};
-
-class Genome
-{
-public:
-    struct InnovationEntry
+    // Singleton class to control InnovationId.
+    class InnovationCounter
     {
-        InnovationId m_id;
-        EdgeId m_edgeId;
-    };
+    public:
+        InnovationCounter() = default;
 
-    struct Node : public NodeBase
-    {
-        virtual float getValue() const override { return m_value; }
-        void setValue(float value) override { m_value = value; }
+        // Returns a new innovation id.
+        InnovationId getNewInnovationId();
+
     protected:
-        float m_value;
+        InnovationCounter(const InnovationCounter&) = delete;
+        void operator=(const InnovationCounter&) = delete;
+
+        InnovationId m_innovationCount = 0;
     };
 
-    using Network = MutableNetwork<Node>;
-    using NetworkPtr = std::shared_ptr<Network>;
-    using InnovationEntries = std::vector<InnovationEntry>;
-
-    struct Cinfo
+    struct Activation
     {
-        uint16_t m_numInputNode;
-        uint16_t m_numOutputNode;
+        using Func = std::function<float(float)>;
+
+        float activate(float value) const { return m_func(value); }
+
+        std::string m_name;
+        Func m_func;
     };
 
-    // Constructor with cinfo. It will construct the minimum dimensional network where there is no hidden node and
-    // all input nodes and output nodes are fully connected.
-    Genome(const Cinfo& cinfo);
+    struct MutationParams
+    {
 
-    // Get innovations of this network. Returned list of innovation entries is sorted by innovation id.
-    inline auto getInnovations() const->const InnovationEntries& { return m_innovations; }
+    };
 
-protected:
-    NetworkPtr m_network;
-    InnovationEntries m_innovations; // A list of innovations sorted by innovation id.
-};
+    // Genome for NEAT
+    class Genome
+    {
+    public:
+        struct InnovationEntry
+        {
+            bool operator<(const InnovationEntry& ie) { return m_id < ie.m_id; }
+
+            InnovationId m_id;
+            EdgeId m_edgeId;
+        };
+
+        struct Node : public NodeBase
+        {
+        public:
+            virtual float getValue() const override;
+            virtual void setValue(float value) override;
+            void setActivation(const Activation* activation) { m_activation = activation; }
+            const std::string& getActivationName() const { return m_activation->m_name; }
+
+        protected:
+            float m_value;
+            const Activation* m_activation;
+        };
+
+        using Network = MutableNetwork<Node>;
+        using NetworkPtr = std::shared_ptr<Network>;
+        using InnovationEntries = std::vector<InnovationEntry>;
+
+        struct Cinfo
+        {
+            uint16_t m_numInputNode;
+            uint16_t m_numOutputNode;
+        };
+
+        // Constructor with cinfo. It will construct the minimum dimensional network where there is no hidden node and
+        // all input nodes and output nodes are fully connected.
+        Genome(const Cinfo& cinfo);
+
+        void mutate(const MutationParams& params, InnovationCounter& innovIdCounter);
+
+        // Get innovations of this network. Returned list of innovation entries is sorted by innovation id.
+        inline auto getInnovations() const->const InnovationEntries& { return m_innovations; }
+
+    protected:
+        NetworkPtr m_network;
+        InnovationEntries m_innovations; // A list of innovations sorted by innovation id.
+    };
+
+
+    class Generation
+    {
+    public:
+        using GenomePtr = std::shared_ptr<const Genome>;
+
+        struct GenomeData
+        {
+        public:
+            void setFitness(float fitness) const { m_fitness = fitness; }
+
+        protected:
+            GenomePtr m_genome;
+            GenomeId m_id;
+            mutable float m_fitness;
+        };
+
+        using Genomes = std::shared_ptr<GenomeData>;
+        using GenerationPtr = std::shared_ptr<Generation>;
+
+        GenerationPtr createNewGeneration() const;
+
+        const Genomes& getGenomes();
+
+    protected:
+        Genomes m_genomes;
+        GenerationId m_id;
+    };
+
+    class NEAT
+    {
+    public:
+        using GenerationPtr = Generation::GenerationPtr;
+
+        void createNewGeneration();
+
+        const Generation& getCurrentGeneration() const;
+
+        void calcFitnessOfCurrentGen();
+        float getMaxFitnessInCurrentGen() const;
+
+    protected:
+        virtual float calcFitness(const Genome& genome) const = 0;
+
+        GenerationPtr m_currentGen;
+        GenerationPtr m_previousGen;
+    };
+}

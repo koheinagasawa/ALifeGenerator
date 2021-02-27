df31a922730674572525705794a2ae5931b857c5
Kohei Nagasawa <k.nagasawa0124@gmail.com>
2021-02-27 20:02:07 +0900

    Added unit test files for DefaultGenomeSelector and SpeciesChampionSelector.

---
+++ Test/UnitTest/DefaultGenomeSelectorTest.cpp
@@ -0,0 +1,15 @@
+/*
+* DefaultGenomeSelectorTest.cpp
+*
+* Copyright (C) 2021 Kohei Nagasawa All Rights Reserved.
+*/
+
+#include <UnitTest/UnitTestPch.h>
+
+#include <NEAT/GeneticAlgorithms/NEAT/Selectors/DefaultGenomeSelector.h>
+
+TEST(DefaultGenomeSelector, CreateSelector)
+{
+    // TODO
+    EXPECT_TRUE(false);
+}
---
+++ Test/UnitTest/SpeciesChampionSelectorTest.cpp
@@ -0,0 +1,15 @@
+/*
+* GenomeTest.cpp
+*
+* Copyright (C) 2021 Kohei Nagasawa All Rights Reserved.
+*/
+
+#include <UnitTest/UnitTestPch.h>
+
+#include <NEAT/GeneticAlgorithms/NEAT/Generators/SpeciesChampionSelector.h>
+
+TEST(SpeciesChampionSelector, SelectChampions)
+{
+    // TODO
+    EXPECT_TRUE(false);
+}

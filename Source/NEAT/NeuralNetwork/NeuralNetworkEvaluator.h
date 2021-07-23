984650dad2e8adb9da8789841206e7d704951068
Kohei Nagasawa <k.nagasawa0124@gmail.com>
2021-07-23 14:05:48 +0900

    Implemented NeuralNetworkEvaluator

--- Source/NEAT/NeuralNetwork/NeuralNetworkEvaluator.h
+++ Source/NEAT/NeuralNetwork/NeuralNetworkEvaluator.h
@@ -23,9 +23,9 @@ public:
     void evaluate(NeuralNetwork<Node, Edge>* network) const;
 
 public:
-    EvaluationType m_type;
-    uint16_t m_evalIterations;
-    float m_convergenceThreshold;
+    EvaluationType m_type = EvaluationType::ITERATION;  // The method to evaluate network.
+    uint16_t m_evalIterations = 10;                     // The maximum number of iteration to run network.
+    float m_convergenceThreshold = 1E-3f;               // Threshold of convergence of output values. Only used for CONVERGE type.
 };
 
 template <typename Node, typename Edge>
@@ -33,23 +33,56 @@ void NeuralNetworkEvaluator::evaluate(NeuralNetwork<Node, Edge>* network) const
 {
     if (network->allowsCircularNetwork())
     {
-        switch (m_type)
+        // Network containing recursion.
+        const bool checkConvergence = m_type == EvaluationType::CONVERGE;
+
+        const NeuralNetwork<Node, Edge>::NodeIds& outputNodes = network->getOutputNodes();
+        const int numOutputNodes = (int)outputNodes.size();
+
+        // Prepare a buffer to store output values of the previous evaluation.
+        std::vector<float> previousOutputVals;
+        if (checkConvergence)
         {
-        case NeuralNetworkEvaluator::EvaluationType::ITERATION:
-        {
-            // TODO
-            break;
+            previousOutputVals.resize(numOutputNodes);
         }
-        case NeuralNetworkEvaluator::EvaluationType::CONVERGE:
-            // TODO
-            break;
-        default:
-            assert(0);
-            break;
+
+        // Run evaluation multiple times.
+        for (uint16_t itr = 0; itr < m_evalIterations; itr++)
+        {
+            network->evaluate();
+
+            if (checkConvergence)
+            {
+                if (itr > 0)
+                {
+                    // Check if the output values have been converged.
+                    bool converged = true;
+                    for (int i = 0; i < numOutputNodes; i++)
+                    {
+                        const float nodeVal = network->getNode(outputNodes[i]).getValue();
+                        converged &= (std::fabs(previousOutputVals[i] - nodeVal) <= m_convergenceThreshold);
+                        previousOutputVals[i] = nodeVal;
+                    }
+
+                    if (converged)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    // Just copy the output values for the first run.
+                    for (int i = 0; i < numOutputNodes; i++)
+                    {
+                        previousOutputVals[i] = network->getNode(outputNodes[i]).getValue();
+                    }
+                }
+            }
         }
     }
     else
     {
+        // Feed forward network. Just evaluate only once.
         network->evaluate();
     }
 }
